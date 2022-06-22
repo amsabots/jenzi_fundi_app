d48684efb796ccaf10f470c03097b5b7bf5133f5
andrew <andrew.mititi@imbank.co.ke>
2022-06-22 16:23:39 +0300

    feature: done with the basic info screen

--- src/config/utils.js
+++ src/config/utils.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import {endpoints} from '../endpoints';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {offline_data} from '../constants';
+import {store} from '../../App';
+import {user_data_actions} from '../store-actions';
 
 axios.defaults.baseURL = endpoints.jenzi_backend + '/jenzi/v1';
 export const update_user_info = user_id => {
@@ -10,10 +12,7 @@ export const update_user_info = user_id => {
     .get(`/fundi/${user_id}`)
     .then(async res => {
       await AsyncStorage.setItem(offline_data.user, JSON.stringify(res.data));
+      store.dispatch(user_data_actions.create_user(res.data));
     })
-    .catch(err =>
-      console.log(
-        `+++++++ [error: user information update action faild ]++++++++=`,
-      ),
-    );
+    .catch(err => console.log(err));
 };
